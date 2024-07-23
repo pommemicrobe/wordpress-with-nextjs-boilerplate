41050ab821fcefdad57f5a227984f4275ff1aa96
Vincent <vincent@Vincents-MBP.lan>
2024-07-23 23:42:17 +0200

    Init

---
+++ .docker/local/config/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+# start nginx
+nginx
+
+# start php-fpm
+php-fpm
---
+++ wp-content/themes/boilerplate/index.php
@@ -0,0 +1,3 @@
+<?php
+
+echo 'Oopssss! I think you must have taken the wrong URL and wanted to <a href="http://localhost:3000">go here</a> instead!';
